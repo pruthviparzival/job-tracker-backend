10180304ad30999d67cf6a690272493a8f5b55ba
pruthviparzival <pruthvirajsharanappa@gmail.com>
2025-04-11 20:09:05 +0530

    update sameSite

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateTokenAndSetCookie = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true, // prevent XSS attacks (cross-site scripting attacks)
-    sameSite: "lax", // CSRF attacks (cross-site request forgery attacks)
+    sameSite: "none", // CSRF attacks (cross-site request forgery attacks)
     secure: process.env.NODE_ENV !== "development", // it becomes secure  during
     //  production(true), in development (false)
   });
